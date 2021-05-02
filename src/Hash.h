cdf45976e7d4ffd4ae327eaa80cfc6acf58481dd
Ben Neal <benneal92@gmail.com>
2021-05-02 16:13:05 -0400

    updated Hash.h

--- src/Hash.h
+++ src/Hash.h
@@ -1,36 +1,13 @@
 #ifndef HASH_H
 #define HASH_H
 
-#include <cstddef> // size_t
+#include <cstddef> 
 
-/**
- * Hash functor to be defined individually for each obj
- */ 
 template<typename T>
 class Hash {
 public:
     size_t operator()(const T&);
 
 };
-
-struct A {
-    std::string name;
-
-    //==
-};
-
-template<>
-class Hash<A> {
-public:
-    size_t operator()(const A& a){
-        size_t hashVal = 0;
-        for( char ch : a.name )
-        hashVal = 37 * hashVal + ch;
-        return hashVal;
-    }
-};
-
-
-
 
 #endif /* HASH_H */
