24894963cbfd33caff14e12cd2b7e922251251d0
Benjamin Liden <benjamin.c.liden@gmail.com>
2021-05-08 13:37:29 -0400

    More iterator tests, fixing edge case behavior

--- src/Driver.cpp
+++ src/Driver.cpp
@@ -12,18 +12,27 @@
 // test declarations
 void testInsert();
 void testRemove();
+void testRemoveMultiple();
 void testLoadFactor();
 void testIterator();
+void testEmptyIterator();
+void testIteratorWithEmptyBeginPosition();
 
 int main() {
 	testInsert();
 	testRemove();
+	testRemoveMultiple();
 	testLoadFactor();
 	testIterator();
+	testEmptyIterator();
+	testIteratorWithEmptyBeginPosition();
 	std::cout << "Tests ran finished without error 🍕" << std::endl;
 	return 0;
 }
 
+/*
+ * Does it insert items?
+ */
 void testInsert() {
 	std::stringstream testLog;
 	std::string const matcher =
@@ -51,9 +60,20 @@ void testInsert() {
 	assert(matcher == testLog.str());
 }
 
+/*
+ * Does it remove one item?
+ */
 void testRemove() {
 	std::stringstream testLog;
-	std::string const matcher =
+
+	HashTable<int> ht;
+	ht.insert(1);
+	ht.insert(2);
+	ht.insert(3);
+	ht.remove(2);
+	testLog << ht;
+
+	std::string const expected =
 			R"(HashTable [
 0>> 
 1>> 
@@ -68,17 +88,46 @@ void testRemove() {
 10>> 
 ] 
 )";
-
-	HashTable<int> ht;
-	ht.insert(1);
-	ht.insert(2);
-	ht.insert(3);
-	ht.remove(2);
-	testLog << ht;
-
-	assert(matcher == testLog.str());
+	assert(expected == testLog.str());
 }
 
+/*
+ * Does it remove multiple of the same item?
+ */
+void testRemoveMultiple() {
+	std::stringstream ss;
+	HashTable<int> ht;
+	for (int i = 0; i < 15; i++) {
+		ht.insert(i);
+	}
+	for (int i = 0; i < 15; i++) {
+		ht.insert(i);
+	}
+	ht.remove(7);
+	ht.remove(11);
+	ss << ht;
+
+	std::string expected =
+			R"(HashTable [
+0>> 0, 0, 
+1>> 
+2>> 9, 6, 3, 9, 6, 3, 
+3>> 
+4>> 12, 12, 
+5>> 5, 2, 5, 2, 
+6>> 
+7>> 14, 8, 14, 8, 
+8>> 1, 1, 
+9>> 
+10>> 13, 10, 4, 13, 10, 4, 
+] 
+)";
+	assert(expected == ss.str());
+}
+
+/*
+ * Does it calculate the load factor correctly?
+ */
 void testLoadFactor() {
 	std::stringstream ss;
 	HashTable<int> ht;
@@ -89,43 +138,76 @@ void testLoadFactor() {
 	assert(ss.str() == "1.36364");
 }
 
+/*
+ * Does it correctly iterate a full list?
+ */
 void testIterator() {
 	std::stringstream ss;
-	std::string check = R"(0,9,6,3,12,5,2,14,11,8,1,13,10,7,4,)";
 	HashTable<int> ht;
 	for (int i = 0; i < 15; i++) {
 		ht.insert(i);
 	}
-	for (auto& item : ht) {
+	for (auto &item : ht) {
 		ss << item << ',';
 	}
-	assert(check == ss.str());
+	std::string expected = R"(0,9,6,3,12,5,2,14,11,8,1,13,10,7,4,)";
+	assert(expected == ss.str());
 }
 
 /*
-    Car c1{2020, 0, "Corolla", "Toyota", new FrontWheelDrive};
-    Car c2{c1};
-    Car c3{2021, 100, "Model S", "Tesla", new AllWheelDrive};
+ * Will the iterator be fine if no items are added?
+ */
+void testEmptyIterator() {
+	std::stringstream ss;
+	HashTable<int> ht;
+	// add NO items at all
+	for (auto &item : ht) {
+		ss << item << ',';
+	}
+	std::string expected = "";
+	assert(expected == ss.str()); // nothing printed. Correct!
+}
 
-    c1 = c3;
-    c1.performDrive();
+/*
+ * Will the iterator find the first good item?
+ */
+void testIteratorWithEmptyBeginPosition() {
+	std::stringstream ss;
+	HashTable<int> ht;
+	ht.insert(5);
+	ht.insert(5);
+	ht.insert(5);
 
-    c2.setSpeed(55);
+	auto iterator = ht.begin();
+	coords expected { 5, 0 }; // iterator should automatically seek to first 5
 
-    std::cout << c1 << '\n';
-    std::cout << (c1 == c2) << '\n';
-    std::cout << "c2 speed " << c2.getSpeed() << '\n';
+	assert(iterator.position == expected);
+}
 
-    c2.setBrandModel("Ford", "Focus");
-    std::cout << c2 << '\n';
-    std::cout << (c1 == c2) << '\n';
+/*
+ Car c1{2020, 0, "Corolla", "Toyota", new FrontWheelDrive};
+ Car c2{c1};
+ Car c3{2021, 100, "Model S", "Tesla", new AllWheelDrive};
 
-    c1.performDrive();
+ c1 = c3;
+ c1.performDrive();
 
-    std::cout << '\n';
+ c2.setSpeed(55);
 
-    c2.setDrive(new RearWheelDrive);
-    c2.performDrive();
+ std::cout << c1 << '\n';
+ std::cout << (c1 == c2) << '\n';
+ std::cout << "c2 speed " << c2.getSpeed() << '\n';
 
-    std::cout << '\n';
-*/
+ c2.setBrandModel("Ford", "Focus");
+ std::cout << c2 << '\n';
+ std::cout << (c1 == c2) << '\n';
+
+ c1.performDrive();
+
+ std::cout << '\n';
+
+ c2.setDrive(new RearWheelDrive);
+ c2.performDrive();
+
+ std::cout << '\n';
+ */
--- src/HashTableIterator.h
+++ src/HashTableIterator.h
@@ -2,10 +2,7 @@
  * HashTableIterator.h
  *
  *  Created on: May 5, 2021
- *      Author: Ben
- *
- *
- *  Warning: this has no testing behind it yet. Beware!
+ *      Author: Benjamin Liden
  *
  *  Using 'long long unsigned int' in this file so it compares cleanly to the std::{vector, list}.size() returns.
  */
@@ -18,22 +15,21 @@
 #include <list>
 #include <stdexcept>
 
+class coords {
+public:
+	long long unsigned int row;
+	long long unsigned int column;
+
+	bool operator==(const coords &incoming) const {
+		return (this->row == incoming.row) && (this->column == incoming.column);
+	}
+	bool operator!=(const coords &incoming) const {
+		return !(*this == incoming);
+	}
+};
+
 template<class T>
 class HashTableIterator {
-
-	class coords {
-	public:
-		long long unsigned int row;
-		long long unsigned int column;
-
-		bool operator==(const coords &incoming) const {
-			return (this->row == incoming.row)
-					&& (this->column == incoming.column);
-		}
-		bool operator!=(const coords &incoming) const {
-			return !(*this == incoming);
-		}
-	};
 
 public:
 	HashTableIterator();
@@ -47,21 +43,27 @@ private:
 	std::vector<std::list<T>> hashTable;
 	coords endPosition;
 	T heldItem;
+
+	bool isValidPosition() const;
+	coords getNextPosition(coords const &position) const;
+	void seekNextValidPosition();
 };
 
 template<class T>
 HashTableIterator<T>::HashTableIterator(std::vector<std::list<T>> &ht, bool end) :
 		hashTable(ht) {
 	// last element of last row
-	long long unsigned int endRow = ht.size() - 1;
+	long long unsigned int endRow = ht.size() - 1; // last good row
 	long long unsigned int endCol = ht.at(endRow).size() + 1; // this represents ONE past the end of the data
 	this->endPosition = coords { endRow, endCol };
 
 	if (end) {
 		this->position = this->endPosition;
 	} else {
-		// beginning
+		// start at beginning
 		this->position = coords { 0, 0 };
+		// then use that or next good coordinate
+		this->seekNextValidPosition();
 	}
 
 }
@@ -87,60 +89,77 @@ T& HashTableIterator<T>::operator*() {
 
 template<class T>
 HashTableIterator<T>& HashTableIterator<T>::operator++() {
-	auto isValid = [&](coords position) {
-		if (position.row >= hashTable.size()) {
-			return false;
-		}
-		std::list<T> row = hashTable.at(position.row);
-		// not empty at the row and column is within bounds
-		return !row.empty() && position.column < row.size();
-	};
-
-	// so, we want to find the next good item...
 	// increment one step to start
-	position.column++;
-	while (!isValid(position)) {
-		bool columnAtEndOfRow;
-		bool noMoreRows;
-		bool notAtEndOfCurrentRow;
-		bool rowEmpty;
-
-		try {
-			columnAtEndOfRow = position.column
-					>= (hashTable.at(position.row).size());
-			noMoreRows = position.row == (hashTable.size());
-			notAtEndOfCurrentRow = position.column
-					< hashTable.at(position.row).size();
-			rowEmpty = hashTable.at(position.row).empty();
-		} catch (std::exception const& e) {
-			// if ANY error while assigning these, return end position
-			position = this->endPosition;
-			break;
-		}
-
-		if ((rowEmpty || columnAtEndOfRow) && noMoreRows) {
-			// if end of row and no more rows, set endPosition
-			position = this->endPosition;
-//			break;
-		} else if (rowEmpty || columnAtEndOfRow /* and more rows exist */) {
-			// if at end of row, move to next and reset column idx
-			position = coords { position.row + 1, 0 };
-		} else if (notAtEndOfCurrentRow) {
-			// happy path... get next column item in list
-			position = coords { position.row, position.column + 1 };
-		} else {
-			// this really shouldn't throw an error, but...
-			throw std::out_of_range("You've gone too far this time.");
-		}
-	}
-
+	this->position.column++;
+	// then find the next good item if that
+	this->seekNextValidPosition();
 	return *this;
 }
 
 template<class T>
 bool HashTableIterator<T>::operator!=(const HashTableIterator<T> &it) const {
 	// just compare the positions
-	return position != it.position;
+	return this->position != it.position;
+}
+
+// Is the given position valid and occupied?
+template<class T>
+bool HashTableIterator<T>::isValidPosition() const {
+	if (this->position.row >= hashTable.size()) {
+		return false;
+	}
+	std::list<T> row = hashTable.at(this->position.row);
+	// not empty at the row and column is within bounds
+	return !row.empty() && this->position.column < row.size();
+}
+
+
+/*
+ * Advance this->position to the next immediate step, regardless of validity.
+ */
+template<class T>
+coords HashTableIterator<T>::getNextPosition(coords const &position) const {
+	bool columnAtEndOfRow;
+	bool lastRow;
+	bool notAtEndOfCurrentRow;
+	bool rowEmpty;
+
+	try {
+		columnAtEndOfRow = position.column
+				>= (hashTable.at(position.row).size());
+		lastRow = position.row == (hashTable.size() - 1);
+		notAtEndOfCurrentRow = position.column
+				< hashTable.at(position.row).size();
+		rowEmpty = hashTable.at(position.row).empty();
+	} catch (std::exception const &e) {
+		// if ANY error while assigning these, return end position
+		return this->endPosition;
+	}
+
+	if ((rowEmpty || columnAtEndOfRow) && lastRow) {
+		// if end of row and no more rows, set endPosition
+		return this->endPosition;
+	} else if (rowEmpty || columnAtEndOfRow /* and more rows exist */) {
+		// if at end of row, move to next and reset column idx
+		return coords { position.row + 1, 0 };
+	} else if (notAtEndOfCurrentRow) {
+		// happy path :) get next column item in list
+		return coords { position.row, position.column + 1 };
+	} else {
+		// this really shouldn't throw an error, but...
+		throw std::out_of_range("You've gone too far this time.");
+	}
+}
+
+/*
+ * Move current coords to next VALID position, inclusive of current position.
+ */
+template<class T>
+void HashTableIterator<T>::seekNextValidPosition() {
+	while (!isValidPosition()
+			&& this->position != this->endPosition) {
+		this->position = this->getNextPosition(this->position);
+	}
 }
 
 #endif /* SRC_HASHTABLEITERATOR_H_ */
